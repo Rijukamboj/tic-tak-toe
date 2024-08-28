6a745eca306c569c67340f4a7caef9a52ea53e39
rijukamboj <rijukamboj2004@gmail.com>
2024-08-28 12:00:07 +0530

    add new files

---
+++ tictok.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetbtn");
+let newBtn = document.querySelector("#newbtn");
+let msgcontainerBtn = document.querySelector(".msgcontainer");
+let msg = document.querySelector("#msg");
+
+let turnO = true ;
+let count = 0;
+ const winPattern = [ 
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+ ];
+ 
+ boxes.forEach((box) => {
+   box.addEventListener("click", () => {
+      if( turnO ){
+         box.innerText = "O";
+         turnO = false;
+      } else {
+         box.innerText = "X";
+         turnO = true;
+      }
+      box.disabled = true;
+        checkWinner();
+      
+   });
+ });
+ 
+ const checkWinner = () => {
+   for (let pattern of winPattern) {
+     let pos1Val = boxes[pattern[0]].innerText;
+     let pos2Val = boxes[pattern[1]].innerText;
+     let pos3Val = boxes[pattern[2]].innerText;
+ 
+     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+       if (pos1Val === pos2Val && pos2Val === pos3Val) {
+         showWinner(pos1Val);
+         
+       }
+     }
+   }
+ };
+const showWinner=(winner) => {
+   msg.innerText =`congratulations , Winner is  ${winner}`;
+   msgcontainerBtn.classList.remove("hide"); 
+   disableBoxes();
+   
+};
+const disableBoxes=() => {
+   for( let box of boxes){
+     box.disabled = true;
+     
+   }
+};
+const resetGame = () =>{
+   turnO = true;
+   count = 0;
+   enableBoxes();
+   msgcontainerBtn.classList.add("hide");
+   
+};
+const enableBoxes = () => {
+   for( let box of boxes){
+     box.disabled = false;
+     box.innerText = " ";
+     
+   }
+}
+
+newBtn.addEventListener("click",resetGame);
+resetbtn.addEventListener("click",resetGame);
+
+
